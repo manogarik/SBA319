f3530f7fec226fdb99595c53bbe982a7fb2d27eb
Manogari Kumaraguru <manogarikumaraguru@gmail.com>
2025-04-24 11:37:10 -0400

    passengerschema

---
+++ models/passengers.mjs
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// Define the schema for learners.
+// Mongoose will add the _id property to your schemas by default.
+const passengerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required : true,
+        
+    },
+    email : {
+        type:String,
+        lowercase:true,
+        match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
+        required:true,
+    },
+    passportNumber : {
+        type:String,
+        required:true,
+        uppercase:true,
+        match: [/^[A-Z0-9]{6,9}$/, 'Please enter a valid passport number']
+    },
+    nationality: {
+        type : String,
+        required :true,
+        
+    }
+    
+})
+
+export default mongoose.model("Passenger", passengerSchema);
